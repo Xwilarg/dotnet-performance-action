08d0c2e2fc43102ae715c66b0f0f3e3e6901418d
Xwilarg <xwilarg@yahoo.fr>
2021-04-13 00:31:44 +0200

    Move performance file as parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh -l
 
+set -e
+
 export IFS=";"
 
-dotnet tool install -g BenchmarkDotNet.Tool
-cd $(ls -d */|head -n 1)
-dotnet run -c Release -- --filter *Sum* --keepfiles
-cd BenchmarkDotNet.Artifacts/results
-cat $(ls *.csv | head -n 1) | while read method b c d e f g h i j k l m n o p q r s t u v w x y z aa ab ac ad ae af ag ah ai aj ak al am an ao ap mean error stddev;
+cat $1 | while read method b c d e f g h i j k l m n o p q r s t u v w x y z aa ab ac ad ae af ag ah ai aj ak al am an ao ap mean error stddev
 do
     echo -e "$method | $mean | $error | $stddev\n"
 done
