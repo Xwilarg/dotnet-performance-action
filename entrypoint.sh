d5cd4213b9ded8c535581def854ad60073a52651
Xwilarg <xwilarg@yahoo.fr>
2021-04-13 00:49:14 +0200

    Fix invalid export

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export IFS=";"
+export IFS=","
 
 cat $1 | while read method b c d e f g h i j k l m n o p q r s t u v w x y z aa ab ac ad ae af ag ah ai aj ak al am an ao ap mean error stddev
 do
